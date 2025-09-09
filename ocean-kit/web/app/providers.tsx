79c6b49bbd152388ad264afb773dd1571cc72f68
Mitya-crypto <9198803600mts@gmail.com>
2025-09-09 05:46:30 -0700

    Update providers.tsx

--- ocean-kit/web/app/providers.tsx
+++ ocean-kit/web/app/providers.tsx
@@ -5,7 +5,7 @@ import dynamic from 'next/dynamic';
 
 // если алиас "@" не настроен, замени на относительный импорт
 const TonConnectProvider = dynamic(
-  () => import('@/components/providers/TonConnectProvider'),
+  () => import('../components/providers/TonConnectProvider'),
   { ssr: false }
 );
 
